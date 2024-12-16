eba3119bdbf3a066c08ad64354e188a170c20ece
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2024-12-17 02:47:24 +0600

    update index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Hey There ! ! <br>
+        This is Shanto.
+    </h1>
+    <?php
+        echo("First PHP code.");
+        echo("<br>");
+         
+    ?>
+
+    <?php
+        //Variable declare 
+        $name = 'Al Shahriar';
+        $id = 221002566 ;
+        echo($name); 
+        echo("<br>");
+        echo($id); 
+        echo("<br>");
+
+        //Arithmatic Oparetion
+        $num1 = 11; 
+        $num2 = 2; 
+        echo("Sumation of 11 $ 2 = ");
+        echo($num1 + $num2);
+        echo("<br>");
+        echo("Mod of 11 $ 2 = ");
+        echo($num1 % $num2);
+        echo("<br>");
+
+        echo("<br>");
+        echo("Thank you for seeing..."); 
+    ?>
+    </div>
+</body>
+</html>
